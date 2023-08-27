7c8f8beec07e51d186ea309a1b7f06b401a745b5
lucas <lxavier.00@outlook.com>
2023-08-27 17:21:40 -0300

    feat: config eslint and editor config setup project

--- nodejs-expresss/mycontacts/src/index.js
+++ nodejs-expresss/mycontacts/src/index.js
@@ -3,15 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('My Contacts!')
-})
-
-app.get('/contacts', (req, res) => {
-  res.send('My Contacts!')
-})
-
-
+  res.send('My Contacts!');
+});
 
 app.listen(3000, () => {
-  console.log('🔥 Server started at http://localhost:3000')
-})
+  console.log('🔥 Server started at http://localhost:3000');
+});
