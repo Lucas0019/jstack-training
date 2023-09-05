8b503c2df4ea52a69084086ba7775e5970a82d95
lucas <lxavier.00@outlook.com>
2023-09-04 22:25:06 -0300

    feat: refactor methods find user api db

--- nodejs-expresss/mycontacts/src/app/repositories/ContactsRepository.js
+++ nodejs-expresss/mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,22 +1,24 @@
-// const { v4 } = require('uuid');
-
 let { contacts } = require('../mocks/contacts');
 
 const db = require('../../database');
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(contacts.find((contact) => contact.email === email)));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+
+    return row;
   }
 
   delete(id) {
